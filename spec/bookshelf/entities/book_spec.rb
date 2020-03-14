87a3c7c57453aa6bd6deaf565c086fb1d34e6a4a
Takahiro <tk.iyei@gmail.com>
2020-03-14 11:32:33 +0900

    entites/controllers/features/viewsのテストを追加

--- spec/bookshelf/entities/book_spec.rb
+++ spec/bookshelf/entities/book_spec.rb
@@ -1,3 +1,6 @@
-RSpec.describe Book, type: :entity do
-  # place your tests here
+RSpec.describe Book do
+  it 'can be initialized with attributes' do
+    book = Book.new(title: 'リーダブルコード')
+    expect(book.title).to eq('リーダブルコード')
+  end
 end
--- spec/web/features/list_books_spec.rb
+++ spec/web/features/list_books_spec.rb
@@ -1,11 +1,22 @@
 require 'features_helper'
 
 RSpec.describe 'List books' do
+  let(:repository) { BookRepository.new }
+  before do
+    repository.clear
+
+    repository.create(title: 'テステス', author: 'テス太郎')
+    repository.create(title: 'テステス2',   author: 'テス二郎')
+  end
+
   it 'displays each book on the page' do
     visit '/books'
 
     within '#books' do
-      expect(page).to have_css('.book', count: 2)
+      expect(page).to have_selector('.book', count: 2), 'Expected to find 2 books'
     end
+
+    expect(page).to have_content "テステス"
+    expect(page).to have_content "テステス2"
   end
 end
